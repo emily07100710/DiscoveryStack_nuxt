440aa92576fc7319a6429df14f442fd1d44d34e1
emily07100710 <yaemily0710@gmail.com>
2026-08-29 01:22:49 +0800

    fix(system-factory): require same-origin invitation acceptance

--- nuxt-app/server/api/system-factory/invitations/accept.post.ts
+++ nuxt-app/server/api/system-factory/invitations/accept.post.ts
@@ -1,8 +1,9 @@
-import { privateSystemFactoryHeaders, strictSystemFactoryBody } from '../../../system-factory/http'
+import { assertSameOriginMutation, privateSystemFactoryHeaders, strictSystemFactoryBody } from '../../../system-factory/http'
 import { validateInvitationAcceptance } from '../../../system-factory/service'
 
 export default defineEventHandler(async event => {
   privateSystemFactoryHeaders(event)
+  assertSameOriginMutation(event)
   const body = await strictSystemFactoryBody(event, ['token', 'password'])
   await validateInvitationAcceptance(body as any)
   throw createError({ statusCode: 503, statusMessage: 'Tenant invitation activation requires the configured server-only Frappe activator.' })
